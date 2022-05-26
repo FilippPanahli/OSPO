80d14fdc3f299dfcdd471f7cc03edad78be92c38
FilippPanahli <f.panahli@mail.ru>
2022-05-26 20:32:41 +0300

    Bat

---
+++ ОСПОскрипт.bat
@@ -0,0 +1,23 @@
+echo off
+md Panahli
+cd Panahli
+md Filipp
+md adb1908
+pause
+echo off
+echo > 11042000.txt
+cd adb1908
+echo > Afilla.txt
+cd.. 
+cd.. 
+pause
+echo off
+del Panahli /S /Q /F
+pause
+echo off
+cd Panahli
+rd Filipp
+rd adb1908
+cd..
+rd Panahli
+
